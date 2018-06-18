5cca4d039249255bb65af2712ae51f59b54e02c3
Alejandro <uo251893@uniovi.es>
2018-06-18 23:07:46 +0200

    Implementado componente SSEemitter

--- src/main/java/inciDashboard/sseEmitter/SSEemitterFacade.java
+++ src/main/java/inciDashboard/sseEmitter/SSEemitterFacade.java
@@ -1,5 +1,12 @@
 package inciDashboard.sseEmitter;
 
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import inciDashboard.entities.Incidencia;
+
 public interface SSEemitterFacade {
+	
+	public SseEmitter RegisterOperator();
+	public void UpdateViews(Incidencia incidencia);
 
 }
---
+++ src/main/java/inciDashboard/sseEmitter/SSEemitterFacadeClass.java
@@ -0,0 +1,41 @@
+package inciDashboard.sseEmitter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import inciDashboard.entities.Incidencia;
+
+public class SSEemitterFacadeClass implements SSEemitterFacade {
+
+	public List<SseEmitter> emitters = Collections.synchronizedList(new ArrayList<SseEmitter>());
+	private long timeout = Long.MAX_VALUE;
+	
+	@Override
+	public SseEmitter RegisterOperator() {
+		SseEmitter emitter = new SseEmitter(timeout);
+		
+		emitter.onCompletion(() -> emitters.remove(emitter));
+		emitter.onTimeout(() -> emitters.remove(emitter));
+		
+		emitters.add(emitter);
+		
+		return emitter;
+	}
+
+	@Override
+	public void UpdateViews(Incidencia incidencia) {
+		for (SseEmitter emitter : emitters) {
+		    try {
+		    	emitter.send(incidencia, MediaType.APPLICATION_JSON);
+		    } catch (IOException e) {
+		    	emitter.complete();
+		    }
+		}
+	}
+
+}
