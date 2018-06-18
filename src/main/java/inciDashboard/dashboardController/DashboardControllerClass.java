bf19bacc165ca742a59f3e01d4211b704a7f1fb3
Alejandro <uo251893@uniovi.es>
2018-06-18 23:10:22 +0200

    Correccion bug

--- src/main/java/inciDashboard/dashboardController/DashboardControllerClass.java
+++ src/main/java/inciDashboard/dashboardController/DashboardControllerClass.java
@@ -1,5 +1,8 @@
 package inciDashboard.dashboardController;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class DashboardControllerClass implements DashboardControllerFacade, NewIncidence {
 
 	@Override
